825c98944c88e5305aae47961a2678691a52d9da
Esther Dhiramo <133733630+EstherDhiramo@users.noreply.github.com>
2024-03-08 12:52:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,3 +16,24 @@ def reminder():    #This is the reminder function, takes current time from the s
             remindersc.title("Reminder!")
             tk.Label(master=remindersc,text="Flight Number "+i+" Not Updated").grid(row=0,column=0)
             remindersc.mainloop()
+deluser(): #This function is responsible for deleting users
+    def deluserback():
+        u=uname.get()        
+        if not(u in admin or u in supervis or u in cancelled):
+            popup=tk.Tk()
+            popup.title("Username Not Found!")
+            tk.Label(master=popup,text="Username not found! Please try again!").grid(row=1,column=1)
+            
+        else:
+            if uname.get()=='admin':
+                    re=tk.Tk()
+                    re.title("Can't Delete Superuser!")
+                    tk.Label(master=re,text="Cannot Delete Superuser!").grid(row=1,column=1)
+                    re.mainloop()
+            elif u in admin:
+                if u!='admin':
+                    del admin[u]
+                    r1=tk.Tk()
+                    r1.title("Success!")
+                    tk.Label(master=r1,text="Admin User Successfully Deleted!").grid(row=1,column=1)
+                    
